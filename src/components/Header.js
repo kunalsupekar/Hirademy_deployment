bfdd84b58c9f982a8c998f9ea0abb7034f7c72fa
Samplereddygithub <reddykambam456@gmail.com>
2024-03-23 11:46:40 +0530

    authenticationcode
    authenticationcode

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 // Header.js
 import React, { useState } from 'react';
 import { Link } from "react-scroll";
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Header() {
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const { loginWithRedirect,logout, user, isAuthenticated} = useAuth0();
 
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
@@ -84,10 +85,26 @@ function Header() {
               </Link>
             </li>
             <li>
-              <button className="getstarted" onClick={handleGetStarted}>
-                Get Started
-              </button>
-            </li>
+            {isAuthenticated ? (
+    <div className="dropdown text-end ms-2">
+      <Link type="button" className="dropdown-toggle btn" data-bs-toggle="dropdown">
+        <p className="text-sm">{user.name}</p>
+      </Link>
+      <ul className="dropdown-menu block">
+         <li>
+          {/* <Link to={"/profile"} className="dropdown-item block px-4 py-2 text-gray-800 hover:bg-gray-500">
+            Profile
+          </Link> */}
+          <Link className="dropdown-itemblock px-4 py-2 text-gray-800 hover:bg-gray-100" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Logout</Link>
+        </li> 
+        {/* Add other dropdown items here if needed */}
+      </ul>
+    </div>
+  ) : (
+    <button className="btn btn-primary-gradient rounded-pill py-2 px-4 ms-3 d-none d-lg-block" onClick={() => loginWithRedirect()}>Get Started</button>
+  )}
+</li>
+
           </ul>
           <i className={`bi ${isNavOpen ? 'bi-x' : 'bi-list'} mobile-nav-toggle`} onClick={toggleNav}></i>
         </nav>
